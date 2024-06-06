c9ae72cf53d5c1581a482defb73269903b6fa183
Anael Medeiros <anael.medeiros@compasso.com.br>
2024-06-06 19:08:00 -0400

    add rotina de cors

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,20 @@ import express from 'express';
 import dotenv from 'dotenv';
 import {getHelps} from './api';
 import morgan from 'morgan';
+import cors from 'cors';
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(morgan('dev'));
+app.use(cors());
 
 app.get('/helps', async (req, res) => {
   try {
     const helps = await getHelps();
     res.json(helps);
   } catch (error) {
+    console.log('error', error);
     res.status(500).json({error: error.message});
   }
 });
